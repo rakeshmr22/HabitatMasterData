afbc78822d57a49ed30601b8b2ebe852c3e45df7
rakeshmr22 <rakesh.shabaraya@capgemini.com>
2017-09-27 16:10:43 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,6 @@ gulp.task("default", function (callback) {
     "01-Copy-Sitecore-License",
     "02-Nuget-Restore",
     "03-Publish-All-Projects",
-    "05-Sync-Unicorn",
     "06-Deploy-Transforms",
 	callback);
 });
